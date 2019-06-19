196e4bbe62e184afb207dfb921d697aaba33f170
Yong Zhou <yong-zh@qq.com>
2019-06-19 17:57:49 +0800

    Client修改

---
+++ DevTools.GuiComm/Wpf/Converters/OppositeBrushConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DevTools.GuiComm.Wpf.Converters
+{
+    public class OppositeBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is Brush brush)
+            {
+                return brush.GetOppositeBrush();
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
